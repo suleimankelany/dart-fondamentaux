5d080e19444e0feca5bcf42f45d21534f995cb4d
Suleiman Kelany <suleiman.kelany@gmail.com>
2021-09-02 15:32:41 +0100

    update null_safety

--- 02-Les-structures-conditionnelles/04-operateur-de-nullite.dart
+++ 02-Les-structures-conditionnelles/04-operateur-de-nullite.dart
@@ -1,3 +1,14 @@
 void main(List<String> args) {
   // Todo : A voir àprès avoir abordée la partir fonction
+
+  isNull(true);
+  isNull(null);
+}
+
+void isNull(bool ?etat) {
+  if (etat == null) {
+    print('Je vaux null');
+  }else {
+     print('Je suis different de Null');
+  }
 }
